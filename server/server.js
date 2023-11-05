292a19dee0dd4fac968c606f1272e8da94dac4ed
sidharth1017 <sidharthv605@gmail.com>
2023-11-05 20:57:24 +0530

    Setup Project

---
+++ server/server.js
@@ -0,0 +1,17 @@
+// Import the required modules
+const http = require('http');
+
+// Define the port number
+const port = 5500;
+
+// Create a simple HTTP server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, this is your Node.js server!');
+});
+
+// Start the server
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
